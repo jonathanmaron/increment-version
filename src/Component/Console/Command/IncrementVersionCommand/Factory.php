eabc08c60c5e3891c618be6b1ab129d45cc9868b
Jonathan Maron <jonathan.maron@theimagingsource.com>
2019-12-06 15:18:54 +0100

    Updated ContainerInterface

--- src/Component/Console/Command/IncrementVersionCommand/Factory.php
+++ src/Component/Console/Command/IncrementVersionCommand/Factory.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace Application\Component\Console\Command\IncrementVersionCommand;
 
-use Interop\Container\ContainerInterface;
+use Psr\Container\ContainerInterface;
 use Symfony\Component\Console\Command\Command;
 
 class Factory
